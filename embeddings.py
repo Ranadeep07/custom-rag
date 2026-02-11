867d0b0129e7ce55911e4be6185e0f8f7a15e8cd
ranadeepbhaduri22 <ranadeep.bhaduri@cloudkaptan.com>
2026-02-11 13:22:46 +0530

    fix: package migration

--- embeddings.py
+++ embeddings.py
@@ -2,9 +2,13 @@
 import os
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
 from config import EMBED_MODEL  # e.g., "gemini-embedding-001"
 
+load_dotenv()
+
 # Initialize client with API key from environment variable
+print("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"));
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def embed(texts):
--- rag.py
+++ rag.py
@@ -1,7 +1,6 @@
 from google import genai
 from google.genai import types
 from config import GEN_MODEL, TOP_K, EMBED_MODEL
-from embeddings import embed
 
 client = genai.Client()
 
